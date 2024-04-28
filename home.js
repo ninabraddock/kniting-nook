37ef4f46a57f805c226d9f09887919c92e595e65
ninabraddock <nina.braddock@uvm.edu>
2024-04-28 23:23:26 +0100

    start on footer

--- home.js
+++ home.js
@@ -16,11 +16,7 @@ productContainers.forEach((item, i) => {
     })
 })
 
-// toggle heart icon 
-// const heartIcon = document.getElementById('heart-icon');
-// const heartBtn = document.getElementById('heart-btn');
-
-
+// select heart icon on each product card
 document.querySelectorAll('.heart-btn').forEach((heartBtn) => {
     heartBtn.addEventListener('click', function() {
         const heartIcon = this.querySelector('.heart-icon');
@@ -28,6 +24,7 @@ document.querySelectorAll('.heart-btn').forEach((heartBtn) => {
     });
 });
 
+// toggle heart icon 
 function toggleHeart(heartIcon) {
     if (heartIcon.classList.contains('far')) {
         heartIcon.classList.remove('far');
@@ -43,41 +40,3 @@ function toggleHeart(heartIcon) {
 }
 
 
-
-// document.querySelectorAll('.heart-btn').forEach((heartBtn) => {
-//     heartBtn.addEventListener('click', function() {
-//         const heartIcon = this.querySelector('.heart-icon');
-//         toggleHeart(heartIcon);
-//     });
-// });
-
-// heartBtn.addEventListener('click', function() {
-//     if (heartIcon.classList.contains('far')) {
-//         heartIcon.classList.remove('far');
-//         heartIcon.classList.add('fas'); // class for solid heart
-//         heartIcon.style.color = '#98002e'; // fill color to red
-//         heartBtn.setAttribute('aria-checked', 'true'); 
-//     } else if (heartIcon.classList.contains('fas')) {
-//         heartIcon.classList.remove('fas');
-//         heartIcon.classList.add('far'); // class for outline heart
-//         heartIcon.style.color = 'rgb(40, 40, 40)'; // change fill color to default
-//         heartBtn.setAttribute('aria-checked', 'false');
-//     }
-// });
-
-// const heartIcon1 = document.getElementById('heart-icon1');
-// const heartBtn1 = document.getElementById('heart-btn1');
-
-// heartBtn1.addEventListener('click', function() {
-//     if (heartIcon1.classList.contains('far')) {
-//         heartIcon1.classList.remove('far');
-//         heartIcon1.classList.add('fas'); // class for solid heart
-//         heartIcon1.style.color = '#98002e'; // fill color to red
-//         heartBtn1.setAttribute('aria-checked', 'true'); 
-//     } else if (heartIcon1.classList.contains('fas')) {
-//         heartIcon1.classList.remove('fas');
-//         heartIcon1.classList.add('far'); // class for outline heart
-//         heartIcon1.style.color = 'rgb(40, 40, 40)'; // change fill color to default
-//         heartBtn1.setAttribute('aria-checked', 'false');
-//     }
-// });
