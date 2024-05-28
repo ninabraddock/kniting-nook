7557c89e66245b73ae7e8074aace27498e2485e3
ninabraddock <nina.braddock@uvm.edu>
2024-05-28 23:44:04 +0100

    working 'you may also like' section btm of best selling

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-// arrow icons
+// Arrow icons
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
