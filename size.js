d9ba01ef2a241574e514dc1a432f6e623ed09239
ninabraddock <nina.braddock@uvm.edu>
2024-08-13 19:59:19 -0600

    clean up

--- size.js
+++ size.js
@@ -50,23 +50,7 @@ btn1.addEventListener('click', function() {
 
 
 
-// const tabs = document.querySelectorAll('[data-tab-target]');
-// const tabContents = document.querySelectorAll('[data-tab-content]');
-
-// tabs.forEach(tab => {
-//     tab.addEventListener('click', () => {
-//         const target = document.querySelector(tab.dataset.tabTarget);
-//         tabContents.forEach(tabContent => {
-//             tabContent.classList.remove('active');
-//         });
-//         tabs.forEach(tab => {
-//             tab.classList.remove('active');
-//         });
-//         tab.classList.add('active');
-//         target.classList.add('active');
-//     });
-// });
-
+// open tabs
 function openTutorial(evt, tutorial) {
     var i, tabContent, tablinks;
 
